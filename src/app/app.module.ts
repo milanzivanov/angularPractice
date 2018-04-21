87f1598fd40ed62608737f54aaa90cd5cae9d4b4
milan.zivanov <milan.zivanov@gmail.com>
2018-04-21 17:04:13 +0200

    popup modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { AgmCoreModule } from '@agm/core';
+import { KorisniciApiService } from './korisnici-api.service';
+import { WeatherService } from './weather.service';
+import { PopupModule } from 'ng2-opd-popup';
+
 
 import { AppComponent } from './app.component';
 import { ChildComponent } from './child/child.component';
 import { GmapComponent } from './gmap/gmap.component';
-
-import { AgmCoreModule } from '@agm/core';
 import { ChartComponent } from './chart/chart.component';
-import { KorisniciApiService } from './korisnici-api.service';
-import { WeatherService } from './weather.service';
 import { MenuComponent } from './menu/menu.component';
 import { SlideMenuComponent } from './slide-menu/slide-menu.component';
 
@@ -29,6 +30,7 @@ import { SlideMenuComponent } from './slide-menu/slide-menu.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    PopupModule.forRoot(),
     AgmCoreModule.forRoot({
       apiKey: 'AIzaSyBLHw2NWSH7JHcriI-H1yfsOegUbpmdNnU'
     })
--- src/app/slide-menu/slide-menu.component.ts
+++ src/app/slide-menu/slide-menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Popup } from 'ng2-opd-popup';
 
 @Component({
   selector: 'app-slide-menu',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlideMenuComponent implements OnInit {
 
-  constructor() {}
+  constructor(private popup: Popup) { }
 
   ngOnInit() {
   }
 
+  ClickButton() {
+
+    this.popup.options = {
+
+      // header: 'Your custom header',
+      color: 'indianred', // red, blue....
+      widthProsentage: 50, // The with of the popou measured by browser width
+      // animationDuration: 1, // in seconds, 0 = no animation
+      // showButtons: true, // You can hide this in case you want to use custom buttons
+      // confirmBtnContent: 'OK', // The text on your confirm button
+      // cancleBtnContent: 'Cancel', // the text on your cancel button
+      confirmBtnClass: 'btn btn-default', // your class for styling the confirm button
+      cancleBtnClass: 'btn btn-default', // you class for styling the cancel button
+      animation: 'fadeInDown' // 'fadeInLeft', 'fadeInRight', 'fadeInUp', 'bounceIn','bounceInDown'
+
+    };
+
+    this.popup.show();
+  }
 }
